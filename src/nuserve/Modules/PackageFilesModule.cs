849a1bf693fa03b6773ca8277f9df919d41de1a3
David Alpert <david@spinthemoose.com>
2011-07-15 00:19:24 -0500

    adding a unit test to verify that published packages are added to the repo

--- src/nuserve/Modules/PackageFilesModule.cs
+++ src/nuserve/Modules/PackageFilesModule.cs
@@ -8,40 +8,49 @@ using NuGet.Server.Infrastructure;
 
 namespace nuserve
 {
-    public class TestModule : NancyModule
+    public interface IZipPackageFactory
+    {
+        IPackage CreatePackageFromRequestBody(Stream requestStream);
+    }
+
+    public class ZipPackageFactory : IZipPackageFactory
+    {
+        public IPackage CreatePackageFromRequestBody(Stream requestStream)
+        {
+            return new ZipPackage(requestStream);
+        }
+    }
+
+    public class PackageFilesModule : NancyModule
     {
         IAuthorizePackageOperations authService;
         IServerPackageRepository packageRepo;
+        IZipPackageFactory zipPackageFactory;
 
         /// <summary>
         /// Initializes a new instance of the TestModule class.
         /// </summary>
         /// <param name="authService"></param>
         /// <param name="settings"></param>
-        public TestModule(IAuthorizePackageOperations authService, IServerPackageRepository packageRepo)
+        public PackageFilesModule(IAuthorizePackageOperations authService, IServerPackageRepository packageRepo, IZipPackageFactory zipPackageFactory)
+            : base("/PackageFiles")
         {
             this.packageRepo = packageRepo;
             this.authService = authService;
+            this.zipPackageFactory = zipPackageFactory;
 
             DefineHandlers();
         }
 
         private void DefineHandlers()
         {
-            Get["/"] = parameters =>
-            {
-                Console.WriteLine("caught request");
-
-                return View["staticview"];
-            };
-
-            Post["/PackageFiles/{apiKey}/nupkg"] = parameters =>
+            Post["/{apiKey}/nupkg"] = parameters =>
             {
                 // Get the api key from the route
                 string apiKey = parameters.apiKey;
 
                 // Build the package from the request body
-                var package = new ZipPackage(Request.Body);
+                var package = zipPackageFactory.CreatePackageFromRequestBody(Request.Body);
 
                 // Make sure they can access this package
                 return Authenticate(apiKey, package.Id, () =>
