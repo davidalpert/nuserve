1242a034a9c9717ab10c5ca69aa3ed25916103f0
Rob Reynolds <ferventcoder@gmail.com>
2011-07-27 06:37:04 -0500

    setting up for the entire script to be run in administrative mode.

--- nuget/tools/chocolateyInstall.ps1
+++ nuget/tools/chocolateyInstall.ps1
@@ -1,33 +1,7 @@
 try {
-  # shut down and remove the service
-  write-host "Shutting down and uninstalling the service if it is installed"
-  try {
-    sc.exe stop nuserve
-    sc.exe delete nuserve
-  } catch {
-    write-host "NOTE: NuServe not installed."
-  }
-  
-  # download and unpack a zip file
-  $nuservePath = "$env:SystemDrive\tools\nuserve"
-  Install-ChocolateyZipPackage 'nuserve' 'https://github.com/downloads/davidalpert/nuserve/nuserve-0.3.0-d776d7aa017.zip' "$nuservePath"
 
-  $nuservePackages = Join-Path $nuservePath "packages"
-  if (![System.IO.Directory]::Exists($nuservePackages)) {[System.IO.Directory]::CreateDirectory($nuservePackages)}
-  $nuserveLogs = Join-Path $nuservePath "logs"
-  if (![System.IO.Directory]::Exists($nuserveLogs)) {[System.IO.Directory]::CreateDirectory($nuserveLogs)}
-  
-  #------- ADDITIONAL SETUP -------#
-  write-host "Installing NuServe"
-  $nuserveExe = Join-Path $nuservePath "nuserve.exe"
-  & $nuserveExe install
-  
-  write-host "Starting NuServe service."
-  try {
-    sc.exe start nuserve
-  } catch {
-    write-host "Not able to start NuServe: $($_.Exception.Message). Other than that, everything was a success."
-  }
+  $psFile = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 'nuserveInstall.ps1'	
+  Start-ChocolateyProcessAsAdmin "& `'$psFile`'"
   
   Write-ChocolateySuccess 'nuserve'
 } catch {
